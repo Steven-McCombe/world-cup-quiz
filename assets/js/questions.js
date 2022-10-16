08178d0e95dfe186d5d6d64f22898b75b5af0753
Steven-McCombe <smccombe93@gmail.com>
2022-10-16 18:28:51 -0400

    Populated Questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,83 @@
+const questions = [
+    {
+        question: "Which country has lost the Mens World Cup Final the most times?",
+        choices: ["a. Germany", "b. Brazil", "c. Ireland", "d. Spain"],
+        answer: "a. Germany"
+    },
+    {
+        question: "Which country will host the 2022 FIFA World Cup?",
+        choices: ["a. Fiji", "b. Denmark", "c. Sweden", "d. Qatar"],
+        answer: "c. Qatar"
+    },
+    {
+        question: "Which country won the 2018 World Cup Final?",
+        choices: ["a. Croatia", "b. France", "c. Netherlands", "d. Italy"],
+        answer: "b. France"
+    },
+    {
+        question: "Which country has won the most Womens World Cup finals?",
+        choices: ["a. United States", "b. Germany", "c. Japan", "d. Norway"],
+        answer: ""
+    },
+    {
+        question: "Before 2022 - How many World Cup finals have Italy won?",
+        choices: ["a. 4", "b. 1", "c. 3", "d. 2"],
+        answer: "a. 4"
+    },
+    {
+        question: "In what year was the first Mens World Cup finals?",
+        choices: ["a. 1926", "b. 1918", "c. 1930", "d. 1934"],
+        answer: "c. 1930"
+    },
+    {
+        question: "How many unique countries have won the Womens World Cup?",
+        choices: ["a. 2", "b. 4", "c. 6", "d. 8"],
+        answer: "b. 4"
+    },
+    {
+        question: "Before 2022 - How many times have England won the world cup final?",
+        choices: ["a. 0", "b. 2", "c. 3", "d. 1"],
+        answer: "d. 1"
+    },
+    {
+        question: "During the 1986 world cup - Who scored the infamous Hand of God goal?",
+        choices: ["a. Diego Maradona", "b. Christiano Ronaldo", "c. David Beckham", "d. George Best"],
+        answer: "a. Diego Maradona"
+    },
+    {
+        question: "Which country pioneered the Viking Clap football chant?",
+        choices: ["a. Denmark", "b. Finland", "c. Norway", "d. Iceland"],
+        answer: "d. Iceland"
+    },
+    {
+        question: "Which player currently holds the record for the most goals scored in the Mens World Cup?",
+        choices: ["a. Miroslav Klose", "b. Pele", "c. Gary Lineker", "d. Ronaldo"],
+        answer: "a. Miroslav Klose"
+    },
+    {
+        question: "What year was the first Womens World cup final?",
+        choices: ["a. 1991", "b. 1995", "c. 2003", "d. 2019"],
+        answer: "a. 1991"
+    }
+    {
+        question: "During the 2006 mens final which French player headbutted the Italian Materazzi?",
+        choices: ["a. Lilian Thuram", "b. Zinedine Zidane", "c. Patrick Viera", "d. Claude Makelele"],
+        answer: "b. Zinedine Zidane"
+    },
+    {
+        question: "Which two brothers are the first to play against each other in a World Cup?",
+        choices: ["a. Neville's", "b. Toure's", "c. Boateng's", "d. De Jong's"],
+        answer: "c. Boateng's"
+    },
+    {
+        question: "How many unique countries have won the Mens World Cup?",
+        choices: ["a. 6", "b. 7", "c. 8", "d. 9"],
+        answer: "d. 9"
+    },
+    {
+        question: "How many unique times have the United States won the Womens World Cup?",
+        choices: ["a. 4", "b. 7", "c. 1", "d. 3"],
+        answer: "a. 4"
+    },
+    
+]
